5c5f54f5456225e90d77c12c698adf25761f4363
IlMike22 <michaelwidlok@yahoo.de>
2022-12-29 10:54:03 +0100

    create translated text field component, put everything together

--- androidApp/src/main/java/com/mind/market/translator_kmm/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/com/mind/market/translator_kmm/android/translate/presentation/TranslateScreen.kt
@@ -1,14 +1,23 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package com.mind.market.translator_kmm.android.translate.presentation
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
 import com.mind.market.translator_kmm.android.translate.presentation.components.LanguageDropDown
 import com.mind.market.translator_kmm.android.translate.presentation.components.SwapLanguagesButton
+import com.mind.market.translator_kmm.android.translate.presentation.components.TranslateTextField
 import com.mind.market.translator_kmm.translate.presentation.TranslateEvent
 import com.mind.market.translator_kmm.translate.presentation.TranslateState
 
@@ -18,11 +27,13 @@ fun TranslateScreen(
     state: TranslateState,
     onEvent: (TranslateEvent) -> Unit,
 ) {
+    val context = LocalContext.current
+
     Scaffold(
         floatingActionButton = {}
     ) { padding ->
         LazyColumn(
-            modifier = Modifier
+            modifier = modifier
                 .fillMaxSize()
                 .padding(padding),
             verticalArrangement = Arrangement.spacedBy(16.dp)
@@ -68,12 +79,50 @@ fun TranslateScreen(
                             onEvent(TranslateEvent.ChooseToLanguage(language))
                         }
                     )
-
-
                 }
+            }
+            item {
+                val clipBoardManager = LocalClipboardManager.current
+                val keyboardController = LocalSoftwareKeyboardController.current
+                TranslateTextField(
+                    fromText = state.fromText,
+                    toText = state.toText,
+                    isTranslating = state.isTranslating,
+                    fromLanguage = state.fromLanguage,
+                    toLanguage = state.toLanguage,
+                    onTranslateClick = {
+                        keyboardController?.hide()
+                        onEvent(TranslateEvent.Translate)
+                    },
+                    onTextChange = { newText ->
+                        onEvent(TranslateEvent.ChangeTranslationText(newText))
+                    },
+                    onCopyClick = { textToCopy ->
+                        clipBoardManager.setText(
+                            buildAnnotatedString {
+                                append(textToCopy)
+                            }
+                        )
+                        Toast.makeText(
+                            context,
+                            context.getString(
+                                com.mind.market.translator_kmm.android.R.string.copied_to_clipboard
+                            ),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    },
+                    onCloseClick = {
+                        onEvent(TranslateEvent.CloseTranslation)
+                    },
+                    onSpeakerClick = {
 
+                    },
+                    onTextFieldClick = {
+                        onEvent(TranslateEvent.EditTranslation)
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
             }
         }
     }
-
 }
--- androidApp/src/main/java/com/mind/market/translator_kmm/android/translate/presentation/components/TranslateTextField.kt
+++ androidApp/src/main/java/com/mind/market/translator_kmm/android/translate/presentation/components/TranslateTextField.kt
@@ -1,17 +1,29 @@
+@file:OptIn(ExperimentalAnimationApi::class)
+
 package com.mind.market.translator_kmm.android.translate.presentation.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import com.mind.market.translator_kmm.android.R
 import com.mind.market.translator_kmm.android.core.theme.LightBlue
 import com.mind.market.translator_kmm.core.presentation.UiLanguage
 
@@ -30,7 +42,116 @@ fun TranslateTextField(
     onTextFieldClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    Box(
+        modifier = modifier
+            .shadow(
+                elevation = 5.dp,
+                shape = RoundedCornerShape(20.dp)
+            )
+            .clip(RoundedCornerShape(20.dp))
+            .gradientSurface()
+            .clickable(onClick = onTextFieldClick)
+            .padding(16.dp)
+    ) {
+        AnimatedContent(
+            targetState = toText
+        ) { toText ->
+            if (toText == null || isTranslating.not()) {
+                IdleTranslateTextField(
+                    fromText = fromText,
+                    isTranslating = isTranslating,
+                    onTextChange = onTextChange,
+                    onTranslateClick = onTranslateClick,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(2f)
+                )
+            } else {
+                TranslatedTextField(
+                    fromText = fromText,
+                    toText = toText,
+                    fromLanguage = fromLanguage,
+                    toLanguage = toLanguage,
+                    onCopyClick = onCopyClick,
+                    onCloseClick = onCloseClick,
+                    onSpeakerClick = onSpeakerClick,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+}
 
+@Composable
+private fun TranslatedTextField(
+    fromText: String,
+    toText: String,
+    fromLanguage: UiLanguage,
+    toLanguage: UiLanguage,
+    onCopyClick: (String) -> Unit,
+    onCloseClick: () -> Unit,
+    onSpeakerClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        LanguageDisplay(language = fromLanguage)
+        Spacer(Modifier.height(16.dp))
+        Text(text = fromText, color = MaterialTheme.colors.onSurface)
+        Spacer(Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            IconButton(onClick = {
+                onCopyClick(fromText)
+            }) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.copy),
+                    contentDescription = stringResource(R.string.copy),
+                    tint = LightBlue
+                )
+            }
+            Spacer(Modifier.width(8.dp))
+            IconButton(onClick = onCloseClick) {
+                Icon(
+                    imageVector = Icons.Rounded.Close,
+                    contentDescription = stringResource(R.string.close),
+                    tint = LightBlue
+                )
+            }
+        }
+
+        Spacer(Modifier.height(16.dp))
+        Divider()
+        Spacer(Modifier.height(16.dp))
+        LanguageDisplay(language = toLanguage)
+        Spacer(Modifier.height(16.dp))
+        Text(text = toText, color = MaterialTheme.colors.onSurface)
+        Spacer(Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            IconButton(onClick = {
+                onCopyClick(toText)
+            }) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.copy),
+                    contentDescription = stringResource(R.string.copy),
+                    tint = LightBlue
+                )
+            }
+            Spacer(Modifier.width(8.dp))
+            IconButton(onClick = onSpeakerClick) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.speaker),
+                    contentDescription = stringResource(R.string.speak),
+                    tint = LightBlue
+                )
+            }
+        }
+    }
 }
 
 @Composable
